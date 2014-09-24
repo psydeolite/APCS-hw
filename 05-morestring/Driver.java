dd2c9452f1732e87747ce226cbb4a9df3d754f36
Mariya Gedrich <maryged@gmail.com>
2014-09-24 09:24:38 -0400

    yay

--- 05-morestring/Driver.java
+++ 05-morestring/Driver.java
@@ -4,5 +4,8 @@ public class Driver {
 	System.out.println(w.nonStart("ada","ire"));
 	System.out.println(w.makeAbba("dancing","queen"));
 	System.out.println(w.diff21(32));
+	System.out.println(w.lastDigit(23,34,45));
+	System.out.println(w.teaParty(4,80));
+	System.out.println(w.nearHundred(98));
     }
 }
--- 05-morestring/StringStuff.java
+++ 05-morestring/StringStuff.java
@@ -14,12 +14,30 @@ public class StringStuff {
 	if (n>21) return result*2;
 	else return result;
     }
+    //working
     public int teaParty(int tea, int candy) {
 	if (tea >=5 && candy >=5) {
 	   if (tea>=2*candy || candy>=2*tea)  {return 2;}
 	   else {return 1;}
 	}
 	else {return 0;}
+
+    }
+    //working
+    public boolean lastDigit(int a, int b, int c) {
+	int a1 = a % 10;
+	int b1 = b % 10;
+	int c1 = c % 10;
+	if (a1 == b1 || a1 == c1 || b1 == c1) return true;
+	else return false;
+    }
+    //working
+    public boolean nearHundred(int n) {
+	if ((Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10)){
+	    return true;
+	} else {
+	    return false;
+	}
+    }
 }
 
-}
