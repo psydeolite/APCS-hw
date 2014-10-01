b911587bb61169ae7ee2c72a8aa5eecec4967950
psydeolite <maryged@gmail.com>
2014-09-30 20:56:09 -0400

    Update Loops.java

--- 06-loops/Loops.java
+++ 06-loops/Loops.java
@@ -12,3 +12,62 @@ public String frontTimes(String str, int n) {
   
 }
 
+//5 min
+public String stringBits(String str) {
+  String r=new String();
+  int n=0;
+  while (n<str.length()) {
+  if (n%2==0) {
+  r+=str.charAt(n);
+  n+=1;
+  }
+  else {
+  n+=1;
+  }
+  }
+  return r;
+}
+
+//10 min
+public String stringYak(String str) {
+  String r=new String();
+  int n=0;
+  while (n<str.length()){
+  if (n+2<str.length() && str.charAt(n)=='y' && str.charAt(n+2)=='k') {
+  n+=3;
+  }
+  else{
+  r+=str.charAt(n);
+  n+=1;
+  }
+  }
+  return r;
+}
+
+//10 min
+public int stringMatch(String a, String b) {
+  int c=0;
+  int n=0;
+  int sh=0;
+  if (a.length()<b.length()){
+  sh=a.length();
+  }
+  else{
+  sh=b.length();
+  }
+  while (n<sh-1){
+  if (a.substring(n,n+2).equals(b.substring(n,n+2))){
+  c+=1;
+  n+=1;
+  }
+  else {
+  n+=1;
+  }
+  }
+  return c;
+
+  
+}
+
+
+
