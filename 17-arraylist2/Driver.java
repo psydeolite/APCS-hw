e8a05f7eaaef61665626110a2896a0d40549e87c
Mariya Gedrich <maryged@gmail.com>
2014-11-16 23:27:37 -0500

    added array version

--- 17-arraylist2/Driver.java
+++ 17-arraylist2/Driver.java
@@ -1,21 +1,52 @@
 import java.util.*;
 public class Driver {
+    //toString for later use
+    public String toString(int[] ar) {
+	String r = "[";
+	for (int i=0;i<ar.length;i++) {
+	    r+=ar[i]+", ";
+	}
+	r=r.substring(0,r.length()-2)+"]";
+	return r;
+    }
     public static void main(String[] args) {
 	Random rand = new Random();
+
+	//creates arraylist
 	ArrayList<Integer> al  = new ArrayList<Integer>();
 	for (int i=1;i<=5;i++) {
 	    al.add(i);
 	}
 	System.out.println(al);
 	
+	//randomizes
 	for (int h=0;h<al.size();h++) {
-	    int x = al.get(rand.nextInt(5));
-	    int indx = al.indexOf(x);
-	    int valh = al.get(h);
+	    int indx = rand.nextInt(5);
+	    int x = al.get(indx);
+      	    int valh = al.get(h);
 	    al.set(h,x);
 	    al.set(indx,valh);
 	}
 	System.out.println(al);
-	    
+
+	//for stringing purposes
+	Driver stringer = new Driver();
+
+	//creates array
+	int[] ale = new int[5];
+	for (int i=0;i<5;i++) {
+	    ale[i]=i+1;
+	}
+       System.out.println(stringer.toString(ale));
+	
+	//randomizes
+	for (int h=0;h<ale.length;h++) {
+	    int indx = rand.nextInt(5);
+	    int x = ale[indx];
+	    int valh = ale[h];
+	    ale[h]=x;
+	    ale[indx] = valh;
+	}
+       	System.out.println(stringer.toString(ale));	    
     }
 }
